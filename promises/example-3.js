4a4c8330c4eeea336d2e8a39d90430443880623b
kavitshah8 <kavit.shah8@gmail.com>
2015-05-08 02:24:12 -0400

    formatting

--- promises/example-3.js
+++ promises/example-3.js
@@ -6,9 +6,9 @@ p1 = new Promise (function(resolve, reject) {
 });
 
 var setteldePromis = p1.then(function(data){
-												return "Success";
-											}).catch(function(err){
-												return "Error";
-											});
+			return "Success";
+		}).catch(function(err){
+			return "Error";
+		});
 
 console.log(setteldePromis._detail); // Will log "Success" in the browser and "undefined" in node.
