464a0392c7ff7936c1d777bd0668082834f2279d
kavitshah8 <kavit.shah8@gmail.com>
2015-05-08 02:26:31 -0400

    exceptions in promises

---
+++ promises/example-4.js
@@ -0,0 +1,14 @@
+// JS exceptions and Promises
+
+var jsonPromise = new Promise(function(resolve, reject) {
+  // JSON.parse throws an error if you feed it some invalid JSON, so this implicitly rejects:
+  resolve(JSON.parse("This ain't JSON"));
+});
+
+jsonPromise.then(function(data) {
+  // This never happens:
+  console.log("It worked!", data);
+}).catch(function(err) {
+  // Instead, this happens:
+  console.log("It failed!", err);
+});
