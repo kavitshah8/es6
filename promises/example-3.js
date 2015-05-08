ddc77f16bae2304f45f43588bdd88e488b36f58a
kavitshah8 <kavit.shah8@gmail.com>
2015-05-08 02:20:02 -0400

    reference to the return value

---
+++ promises/example-3.js
@@ -0,0 +1,14 @@
+var p1;
+var settledPromise;
+
+p1 = new Promise (function(resolve, reject) {
+	resolve();
+});
+
+var setteldePromis = p1.then(function(data){
+												return "Success";
+											}).catch(function(err){
+												return "Error";
+											});
+
+console.log(setteldePromis._detail); // Will log "Success" in the browser and "undefined" in node.
